56aa72edbf60f17108cd1ec45d19a84e8ae05985
Tiago Felipe <tiagofsv95@gmail.com>
2020-01-28 12:58:25 -0300

    Adding fontawesome for future implementations

--- src/components/DevItem/index.js
+++ src/components/DevItem/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
 import './style.css';
 
 function DevItem({ dev }){
